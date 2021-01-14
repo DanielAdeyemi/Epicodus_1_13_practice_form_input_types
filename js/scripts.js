adc5162ce3724d4d6554013c2b0273feacfd3b3a
DanielAdeyemi <adeyemidany@gmail.com>
2021-01-13 20:33:50 -0800

    change html to ensure proper running of js for taking input from radio form

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
     let food = $("#food").val();
-    $(".alert").text(food);
+    let sport = $("input:radio[name=sport]:checked").val();
+    $(".alert").text(sport);
     $(".output").show();
     event.preventDefault();
   });
