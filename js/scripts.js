df2f6d364733fe42c1ca8aa16eb3f63e1707e82b
DanielAdeyemi <adeyemidany@gmail.com>
2021-01-13 20:43:53 -0800

    make alert to display for user and also separate bussiness and UI logics

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,16 @@
+function outcome(food, sport, dob) {
+  let sentence = "I see you were born " + dob + ". Also looks like you woudn't mind to take some " + food + ", but it doesn't matter, because you like " + sport + "!";
+  $(".alert").text(sentence);
+  $(".output").show();
+}
+
+
 $(document).ready(function() {
   $("form").submit(function(event) {
     let food = $("#food").val();
     let sport = $("input:radio[name=sport]:checked").val();
     let dob = $("#born").val()
-    $(".alert").text(dob);
-    $(".output").show();
+    outcome(food, sport, dob);
     event.preventDefault();
   });
 });
