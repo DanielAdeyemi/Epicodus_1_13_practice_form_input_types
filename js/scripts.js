2c871d6a6cb67b6632970d422b43afb8ffc033aa
DanielAdeyemi <adeyemidany@gmail.com>
2021-01-13 20:26:47 -0800

    add functional to read data from user submission on food (from select box)

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
     let food = $("#food").val();
-    $(".output").text(food);
+    $(".alert").text(food);
     $(".output").show();
     event.preventDefault();
-  })
+  });
 });
